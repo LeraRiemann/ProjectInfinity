9007f26e7e3f479517b4044853c86167be61e7af
Lera Riemann <leramipt@gmail.com>
2023-07-18 19:09:27 +0300

    compatibility update on fire

--- src/main/java/net/lerariemann/infinity/mixin/AbstractFireBlockMixin.java
+++ src/main/java/net/lerariemann/infinity/mixin/AbstractFireBlockMixin.java
@@ -3,12 +3,14 @@ package net.lerariemann.infinity.mixin;
 import net.minecraft.block.AbstractFireBlock;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractFireBlock.class)
 public class AbstractFireBlockMixin {
-    @Overwrite
-    private static boolean isOverworldOrNether(World world) {
-        return world.getRegistryKey() != World.END;
+    @Inject(method = "isOverworldOrNether(Lnet/minecraft/world/World;)Z", at = @At("RETURN"), cancellable = true)
+    private static void injected(World world, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(cir.getReturnValue() || world.getRegistryKey().toString().contains("infinity"));
     }
 }
