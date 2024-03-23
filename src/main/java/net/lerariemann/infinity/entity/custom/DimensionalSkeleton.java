255d11862012e7d68b78156f8b89b8c0843c9751
Lera Riemann <leramipt@gmail.com>
2024-03-23 22:49:27 +0300

    dimensional skeletons done

--- src/main/java/net/lerariemann/infinity/entity/custom/DimensionalSkeleton.java
+++ src/main/java/net/lerariemann/infinity/entity/custom/DimensionalSkeleton.java
@@ -8,14 +8,14 @@ import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
-import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.SkeletonEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.potion.Potion;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.potion.PotionUtil;
+import net.minecraft.potion.Potions;
 import net.minecraft.registry.Registries;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
@@ -27,6 +27,7 @@ import java.util.*;
 
 public class DimensionalSkeleton extends SkeletonEntity implements TintableEntity {
     private static final TrackedData<Integer> effect = DataTracker.registerData(DimensionalSkeleton.class, TrackedDataHandlerRegistry.INTEGER);
+    private static final TrackedData<Integer> duration = DataTracker.registerData(DimensionalSkeleton.class, TrackedDataHandlerRegistry.INTEGER);
     public DimensionalSkeleton(EntityType<? extends SkeletonEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -40,6 +41,7 @@ public class DimensionalSkeleton extends SkeletonEntity implements TintableEntit
             if (e.getCategory().equals(StatusEffectCategory.HARMFUL)) a.add(e);
         });
         this.setEffect(a.get(r.nextInt(a.size())));
+        this.setDuration(r.nextInt(200));
         return super.initialize(world, difficulty, spawnReason, entityData, entityNbt);
     }
 
@@ -47,6 +49,7 @@ public class DimensionalSkeleton extends SkeletonEntity implements TintableEntit
     protected void initDataTracker() {
         super.initDataTracker();
         this.dataTracker.startTracking(effect, StatusEffect.getRawId(StatusEffects.POISON));
+        this.dataTracker.startTracking(duration, 200);
     }
 
     public void setEffectRaw(int c) {
@@ -61,20 +64,39 @@ public class DimensionalSkeleton extends SkeletonEntity implements TintableEntit
     public StatusEffect getEffect() {
         return StatusEffect.byRawId(getEffectRaw());
     }
+    public void setDuration(int i) {
+        this.dataTracker.set(duration, i);
+    }
+    public int getDuration() {
+        return this.dataTracker.get(duration);
+    }
     @Override
     public void writeCustomDataToNbt(NbtCompound nbt) {
         super.writeCustomDataToNbt(nbt);
         nbt.putInt("effect", this.getEffectRaw());
+        nbt.putInt("duration", this.getDuration());
     }
     @Override
     public void readCustomDataFromNbt(NbtCompound nbt) {
         super.readCustomDataFromNbt(nbt);
         this.setEffectRaw(nbt.getInt("effect"));
+        this.setDuration(nbt.getInt("duration"));
+    }
+
+    public static ItemStack setPotion(ItemStack stack, int effect, int duration) {
+        NbtList effects = new NbtList();
+        NbtCompound compound = new NbtCompound();
+        compound.putInt("Id", effect);
+        compound.putInt("Duration", duration);
+        effects.add(compound);
+        PotionUtil.setPotion(stack, Potions.WATER);
+        stack.getOrCreateNbt().put("CustomPotionEffects", effects);
+        return stack;
     }
 
     @Override
     public ItemStack getProjectileType(ItemStack stack) {
-        return PotionUtil.setPotion(Items.TIPPED_ARROW.getDefaultStack(), new Potion(new StatusEffectInstance(this.getEffect(), 1800)));
+        return setPotion(Items.TIPPED_ARROW.getDefaultStack(), this.getEffectRaw(), this.getDuration());
     }
 
     @Override
