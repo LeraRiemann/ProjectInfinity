d174d4c98e36d4bbb2b5c5b07b9c2d3291db5204
Cassian Godsted <cassian@godsted.com>
2024-10-12 17:51:35 -0400

    Temporary disable registry supplier for features

--- common/src/main/java/net/lerariemann/infinity/features/ModFeatures.java
+++ common/src/main/java/net/lerariemann/infinity/features/ModFeatures.java
@@ -1,38 +1,42 @@
 package net.lerariemann.infinity.features;
 
-import dev.architectury.registry.registries.DeferredRegister;
-import dev.architectury.registry.registries.RegistrySupplier;
 import net.lerariemann.infinity.InfinityMod;
-import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.FeatureConfig;
 import net.minecraft.world.gen.feature.SingleStateFeatureConfig;
 
-import static net.lerariemann.infinity.InfinityMod.MOD_ID;
+import static net.lerariemann.infinity.PlatformMethods.freeze;
+import static net.lerariemann.infinity.PlatformMethods.unfreeze;
 
 public class ModFeatures {
-    public static RegistrySupplier<RandomEndIslandFeature> RANDOM_END_ISLAND;
-    public static RegistrySupplier<RandomDungeonFeature> RANDOM_DUNGEON;
-    public static RegistrySupplier<RandomColumnsFeature> RANDOM_COLUMNS;
-    public static RegistrySupplier<RandomMushroomFeature> RANDOM_FLAT_MUSHROOM;
-    public static RegistrySupplier<RandomRoundMushroomFeature> RANDOM_ROUND_MUSHROOM;
-    public static RegistrySupplier<RandomCeilingBlobFeature> RANDOM_CEILING_BLOB;
-    public static RegistrySupplier<RandomCubeFeature> RANDOM_CUBE;
-    public static RegistrySupplier<RandomShapeFeature> RANDOM_STAR;
-    public static RegistrySupplier<TextFeature> RANDOM_TEXT;
+    public static Feature<SingleStateFeatureConfig> RANDOM_END_ISLAND;
+    public static Feature<RandomDungeonFeatureConfig> RANDOM_DUNGEON;
+    public static Feature<RandomColumnsFeatureConfig> RANDOM_COLUMNS;
+    public static Feature<RandomMushroomFeatureConfig> RANDOM_FLAT_MUSHROOM;
+    public static Feature<RandomMushroomFeatureConfig> RANDOM_ROUND_MUSHROOM;
+    public static Feature<RandomCeilingBlobFeatureConfig> RANDOM_CEILING_BLOB;
+    public static Feature<RandomCubeFeatureConfig> RANDOM_CUBE;
+    public static Feature<RandomShapeFeatureConfig> RANDOM_STAR;
+    public static Feature<TextFeatureConfig> RANDOM_TEXT;
 
-    public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(MOD_ID, RegistryKeys.FEATURE);
+    public static <C extends FeatureConfig, F extends Feature<C>> F register(String name, F feature) {
+        return Registry.register(Registries.FEATURE, name, feature);
+    }
 
     public static void registerFeatures() {
         InfinityMod.LOGGER.debug("Registering features for " + InfinityMod.MOD_ID);
-        RANDOM_END_ISLAND = FEATURES.register("random_end_island", () -> new RandomEndIslandFeature(SingleStateFeatureConfig.CODEC));
-        RANDOM_DUNGEON = FEATURES.register("random_dungeon",() -> new RandomDungeonFeature(RandomDungeonFeatureConfig.CODEC));
-        RANDOM_COLUMNS = FEATURES.register("random_columns",() -> new RandomColumnsFeature(RandomColumnsFeatureConfig.CODEC));
-        RANDOM_FLAT_MUSHROOM = FEATURES.register("random_flat_mushroom",() -> new RandomFlatMushroomFeature(RandomMushroomFeatureConfig.CODEC));
-        RANDOM_ROUND_MUSHROOM = FEATURES.register("random_round_mushroom",() -> new RandomRoundMushroomFeature(RandomMushroomFeatureConfig.CODEC));
-        RANDOM_CEILING_BLOB = FEATURES.register("random_ceiling_blob",() -> new RandomCeilingBlobFeature(RandomCeilingBlobFeatureConfig.CODEC));
-        RANDOM_CUBE = FEATURES.register("random_cube",() -> new RandomCubeFeature(RandomCubeFeatureConfig.CODEC));
-        RANDOM_STAR = FEATURES.register("random_shape",() -> new RandomShapeFeature(RandomShapeFeatureConfig.CODEC));
-        RANDOM_TEXT = FEATURES.register("random_text",() -> new TextFeature(TextFeatureConfig.CODEC));
-        FEATURES.register();
+        unfreeze(Registries.FEATURE);
+        RANDOM_END_ISLAND = register("random_end_island", new RandomEndIslandFeature(SingleStateFeatureConfig.CODEC));
+        RANDOM_DUNGEON = register("random_dungeon", new RandomDungeonFeature(RandomDungeonFeatureConfig.CODEC));
+        RANDOM_COLUMNS = register("random_columns", new RandomColumnsFeature(RandomColumnsFeatureConfig.CODEC));
+        RANDOM_FLAT_MUSHROOM = register("random_flat_mushroom", new RandomFlatMushroomFeature(RandomMushroomFeatureConfig.CODEC));
+        RANDOM_ROUND_MUSHROOM = register("random_round_mushroom", new RandomRoundMushroomFeature(RandomMushroomFeatureConfig.CODEC));
+        RANDOM_CEILING_BLOB = register("random_ceiling_blob", new RandomCeilingBlobFeature(RandomCeilingBlobFeatureConfig.CODEC));
+        RANDOM_CUBE = register("random_cube", new RandomCubeFeature(RandomCubeFeatureConfig.CODEC));
+        RANDOM_STAR = register("random_shape", new RandomShapeFeature(RandomShapeFeatureConfig.CODEC));
+        RANDOM_TEXT = register("random_text", new TextFeature(TextFeatureConfig.CODEC));
+        freeze(Registries.FEATURE);
     }
 }
