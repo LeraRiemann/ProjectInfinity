63c8b85db2a270668935f1a9658ffdea0fd681b2
Cassian Godsted <cassian@godsted.com>
2024-10-12 13:34:29 -0400

    Port Sound Events to deferred

--- common/src/main/java/net/lerariemann/infinity/var/ModSounds.java
+++ common/src/main/java/net/lerariemann/infinity/var/ModSounds.java
@@ -1,11 +1,15 @@
 package net.lerariemann.infinity.var;
 
+import dev.architectury.registry.registries.DeferredRegister;
 import net.lerariemann.infinity.InfinityMod;
+import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 
+import static net.lerariemann.infinity.InfinityMod.MOD_ID;
 import static net.lerariemann.infinity.PlatformMethods.freeze;
 import static net.lerariemann.infinity.PlatformMethods.unfreeze;
 
@@ -14,12 +18,10 @@ public class ModSounds {
     public static final Identifier IVORY_MUSIC_CHALLENGER = InfinityMod.getId("music.ivory.challenger");
     public static SoundEvent IVORY_MUSIC_HOPE_EVENT = SoundEvent.of(IVORY_MUSIC_HOPE);
     public static SoundEvent IVORY_MUSIC_CHALLENGER_EVENT = SoundEvent.of(IVORY_MUSIC_CHALLENGER);
-
+    public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(MOD_ID, RegistryKeys.SOUND_EVENT);
 
     public static void registerSounds(){
-        unfreeze(Registries.SOUND_EVENT);
-        Registry.register(Registries.SOUND_EVENT, IVORY_MUSIC_HOPE, IVORY_MUSIC_HOPE_EVENT);
-        Registry.register(Registries.SOUND_EVENT, IVORY_MUSIC_CHALLENGER, IVORY_MUSIC_CHALLENGER_EVENT);
-        freeze(Registries.SOUND_EVENT);
+        SOUNDS.register(IVORY_MUSIC_HOPE, () -> IVORY_MUSIC_HOPE_EVENT);
+        SOUNDS.register(IVORY_MUSIC_CHALLENGER, () -> IVORY_MUSIC_CHALLENGER_EVENT);
     }
 }
