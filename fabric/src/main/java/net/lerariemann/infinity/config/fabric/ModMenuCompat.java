02d9e08cb4d524c67a2d8c9f06c93efff5adf32e
Cassian Godsted <cassian@godsted.com>
2024-10-14 12:30:05 -0400

    Standardized mod loaded checks

--- fabric/src/main/java/net/lerariemann/infinity/config/fabric/ModMenuCompat.java
+++ fabric/src/main/java/net/lerariemann/infinity/config/fabric/ModMenuCompat.java
@@ -4,11 +4,13 @@ import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.fabricmc.loader.api.FabricLoader;
 
+import static net.lerariemann.infinity.PlatformMethods.isModLoaded;
+
 public class ModMenuCompat implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         //Display Cloth Config screen if mod present, else error.
-        if (FabricLoader.getInstance().isModLoaded("cloth-config")) return new ModConfigFactory();
+        if (isModLoaded("cloth-config")) return new ModConfigFactory();
         else {
             return parent -> null;
         }
