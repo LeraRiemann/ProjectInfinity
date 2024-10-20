e21dc8e3a308a125bf305007975cd202e1d8fcbf
Cassian Godsted <cassian@godsted.com>
2024-10-20 12:00:22 -0400

    actual fix for poi

--- common/src/main/java/net/lerariemann/infinity/var/ModPoi.java
+++ common/src/main/java/net/lerariemann/infinity/var/ModPoi.java
@@ -1,5 +1,6 @@
 package net.lerariemann.infinity.var;
 
+import com.google.common.collect.ImmutableSet;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.lerariemann.infinity.InfinityMod;
@@ -21,8 +22,8 @@ public class ModPoi {
 
     public static void registerPoi() {
         InfinityMod.LOGGER.debug("Registering POI for " + InfinityMod.MOD_ID);
-        NEITHER_PORTAL = POI_TYPES.register("neither_portal", () -> new PointOfInterestType(Collections.singleton(ModBlocks.NEITHER_PORTAL.get().getDefaultState()), 0, 1));
-        NEITHER_PORTAL_KEY = RegistryKey.of(RegistryKeys.POINT_OF_INTEREST_TYPE, InfinityMod.getId("neither_portal"));
+        NEITHER_PORTAL = POI_TYPES.register("neither_portal", () -> new PointOfInterestType(ImmutableSet.copyOf(ModBlocks.NEITHER_PORTAL.get().getStateManager().getStates()), 0, 1));
         POI_TYPES.register();
+        NEITHER_PORTAL_KEY = RegistryKey.of(RegistryKeys.POINT_OF_INTEREST_TYPE, InfinityMod.getId("neither_portal"));
     }
 }
