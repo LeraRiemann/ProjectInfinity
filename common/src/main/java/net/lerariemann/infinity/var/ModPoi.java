00508d96b5e041b1320f4b40712fd86fb1476542
Cassian Godsted <cassian@godsted.com>
2024-10-21 14:08:22 -0400

    Reimplement dedicated Fabric Poi system

--- common/src/main/java/net/lerariemann/infinity/var/ModPoi.java
+++ common/src/main/java/net/lerariemann/infinity/var/ModPoi.java
@@ -1,6 +1,8 @@
 package net.lerariemann.infinity.var;
 
 import com.google.common.collect.ImmutableSet;
+import dev.architectury.injectables.annotations.ExpectPlatform;
+import dev.architectury.platform.Platform;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.lerariemann.infinity.InfinityMod;
@@ -9,21 +11,30 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.world.poi.PointOfInterestType;
 
-import java.util.Collections;
-
 import static net.lerariemann.infinity.InfinityMod.MOD_ID;
 
 public class ModPoi {
     public static RegistrySupplier<PointOfInterestType> NEITHER_PORTAL;
     public static RegistryKey<PointOfInterestType> NEITHER_PORTAL_KEY;
 
-    public static final DeferredRegister<PointOfInterestType> POI_TYPES = DeferredRegister.create(MOD_ID, RegistryKeys.POINT_OF_INTEREST_TYPE);
-
-
     public static void registerPoi() {
         InfinityMod.LOGGER.debug("Registering POI for " + InfinityMod.MOD_ID);
+        if (Platform.isFabric()) registerPoiFabric();
+        else registerPoiArchitectury();
+        NEITHER_PORTAL_KEY = RegistryKey.of(RegistryKeys.POINT_OF_INTEREST_TYPE, InfinityMod.getId("neither_portal"));
+
+    }
+
+    // Deferred handling of Poi through Architectury API, works on NeoForge.
+    public static void registerPoiArchitectury() {
+        final DeferredRegister<PointOfInterestType> POI_TYPES = DeferredRegister.create(MOD_ID, RegistryKeys.POINT_OF_INTEREST_TYPE);
         NEITHER_PORTAL = POI_TYPES.register("neither_portal", () -> new PointOfInterestType(ImmutableSet.copyOf(ModBlocks.NEITHER_PORTAL.get().getStateManager().getStates()), 0, 1));
         POI_TYPES.register();
-        NEITHER_PORTAL_KEY = RegistryKey.of(RegistryKeys.POINT_OF_INTEREST_TYPE, InfinityMod.getId("neither_portal"));
+    }
+
+    // Legacy handling of Poi on Fabric, as Architectury Poi are not correctly registered.
+    @ExpectPlatform
+    public static void registerPoiFabric() {
+        throw new AssertionError();
     }
 }
