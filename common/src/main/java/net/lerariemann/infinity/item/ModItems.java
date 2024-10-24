516a5e660e8f4f849bbefe3edc435a8546aa2ff7
Cassian Godsted <cassian@godsted.com>
2024-10-24 14:34:22 -0400

    Helper for registering block items

--- common/src/main/java/net/lerariemann/infinity/item/ModItems.java
+++ common/src/main/java/net/lerariemann/infinity/item/ModItems.java
@@ -5,6 +5,7 @@ import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.lerariemann.infinity.block.ModBlocks;
 import net.lerariemann.infinity.var.ModComponentTypes;
+import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -17,25 +18,21 @@ import static net.lerariemann.infinity.InfinityMod.MOD_ID;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, RegistryKeys.ITEM);
-    public static final RegistrySupplier<Item> BOOK_BOX_ITEM = ITEMS.register("book_box", () ->
-            new BlockItem(ModBlocks.BOOK_BOX.get(), new Item.Settings().arch$tab(ItemGroups.FUNCTIONAL)));
-    public static final RegistrySupplier<Item> ALTAR_ITEM = ITEMS.register("altar", () ->
-            new BlockItem(ModBlocks.ALTAR.get(), new Item.Settings().arch$tab(ItemGroups.FUNCTIONAL)));
-    public static final RegistrySupplier<Item> TIME_BOMB_ITEM = ITEMS.register("timebomb", () ->
-            new BlockItem(ModBlocks.TIME_BOMB.get(), new Item.Settings().arch$tab(ItemGroups.OPERATOR)));
+
+    public static final RegistrySupplier<Item> BOOK_BOX_ITEM = registerSimpleBlockItem(ModBlocks.BOOK_BOX, ItemGroups.FUNCTIONAL);
+    public static final RegistrySupplier<Item> ALTAR_ITEM = registerSimpleBlockItem(ModBlocks.ALTAR, ItemGroups.FUNCTIONAL);
+    public static final RegistrySupplier<Item> TIME_BOMB_ITEM = registerSimpleBlockItem(ModBlocks.TIME_BOMB, ItemGroups.OPERATOR);
+    
     public static final RegistrySupplier<Item> TRANSFINITE_KEY = ITEMS.register("key", () ->
             new TransfiniteKeyItem(new Item.Settings()
                     .component(ModComponentTypes.KEY_DESTINATION.get(), Identifier.of("minecraft:random"))
                     .arch$tab(ItemGroups.INGREDIENTS)));
+
+    public static RegistrySupplier<Item> registerSimpleBlockItem(RegistrySupplier<Block> block, RegistryKey<ItemGroup> group) {
+       return ITEMS.register(block.getId(), () -> new BlockItem(block.get(), new Item.Settings().arch$tab(group)));
+    }
+
     public static void registerModItems() {
         ITEMS.register();
-    }
-
-    @ExpectPlatform
-    public static void addAfter(RegistryKey<ItemGroup> functional, Item addBefore, Item item) {
-    }
-
-    @ExpectPlatform
-    public static void add(RegistryKey<ItemGroup> functional, Item item) {
     }
 }
