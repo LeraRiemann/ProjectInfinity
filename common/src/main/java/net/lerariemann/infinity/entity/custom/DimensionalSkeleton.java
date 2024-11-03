33cf29f5fe7e3d789b22759ddffdb73cf3a427ee
Cassian Godsted <cassian@godsted.com>
2024-11-03 14:17:01 -0500

    Skeleton change backports

--- common/src/main/java/net/lerariemann/infinity/entity/custom/DimensionalSkeleton.java
+++ common/src/main/java/net/lerariemann/infinity/entity/custom/DimensionalSkeleton.java
@@ -23,8 +23,10 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.LocalDifficulty;
@@ -39,11 +41,31 @@ public class DimensionalSkeleton extends SkeletonEntity implements TintableEntit
     private static final TrackedData<String> effect = DataTracker.registerData(DimensionalSkeleton.class, TrackedDataHandlerRegistry.STRING);
     private static final TrackedData<Integer> color = DataTracker.registerData(DimensionalSkeleton.class, TrackedDataHandlerRegistry.INTEGER);
     private static final TrackedData<Integer> duration = DataTracker.registerData(DimensionalSkeleton.class, TrackedDataHandlerRegistry.INTEGER);
-    public static final Map<String, String> effect_lookup = Map.ofEntries(Map.entry("unluck", "luck"), Map.entry("bad_omen", "hero_of_the_village"),
-            Map.entry("blindness", "night_vision"), Map.entry("darkness", "night_vision"), Map.entry("glowing", "invisibility"),
-            Map.entry("hunger", "saturation"), Map.entry("levitation", "slow_falling"), Map.entry("mining_fatigue", "haste"),
-            Map.entry("poison", "regeneration"), Map.entry("slowness", "speed"), Map.entry("weakness", "strength"),
-            Map.entry("wither", "regeneration"));
+    public static Map<String, String> effect_lookup = Map.ofEntries(
+            Map.entry("minecraft:bad_omen", "minecraft:hero_of_the_village"),
+            Map.entry("minecraft:darkness", "minecraft:night_vision"),
+            Map.entry("minecraft:blindness", "minecraft:night_vision"),
+            Map.entry("minecraft:glowing", "minecraft:invisibility"),
+            Map.entry("minecraft:hunger", "minecraft:saturation"),
+            Map.entry("minecraft:levitation", "minecraft:slow_falling"),
+            Map.entry("minecraft:mining_fatigue", "minecraft:haste"),
+            Map.entry("minecraft:poison", "minecraft:regeneration"),
+            Map.entry("minecraft:slowness", "minecraft:speed"),
+            Map.entry("minecraft:weakness", "minecraft:strength"),
+            Map.entry("minecraft:wither", "minecraft:regeneration"),
+            Map.entry("minecraft:instant_damage", "minecraft:instant_health"),
+            Map.entry("minecraft:instant_health", "minecraft:instant_damage"),
+            Map.entry("minecraft:luck", "minecraft:unluck"),
+            Map.entry("minecraft:hero_of_the_village", "minecraft:bad_omen"),
+            Map.entry("minecraft:night_vision", "minecraft:darkness"),
+            Map.entry("minecraft:invisibility", "minecraft:glowing"),
+            Map.entry("minecraft:saturation", "minecraft:hunger"),
+            Map.entry("minecraft:slow_falling", "minecraft:levitation"),
+            Map.entry("minecraft:haste", "minecraft:mining_fatigue"),
+            Map.entry("minecraft:regeneration", "minecraft:poison"),
+            Map.entry("minecraft:speed", "minecraft:slowness"),
+            Map.entry("minecraft:strength", "minecraft:weakness"));
+
     public DimensionalSkeleton(EntityType<? extends SkeletonEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -72,7 +94,7 @@ public class DimensionalSkeleton extends SkeletonEntity implements TintableEntit
         Identifier e = a.get(r.nextInt(a.size()));
         this.setEffect(e);
         this.setColorRaw(Objects.requireNonNull(reg.get(e)).getColor());
-        this.setDuration(r.nextInt(200));
+        this.setDuration(r.nextInt(600));
         return super.initialize(world, difficulty, spawnReason, entityData, entityNbt);
     }
 
@@ -84,16 +106,9 @@ public class DimensionalSkeleton extends SkeletonEntity implements TintableEntit
         this.dataTracker.startTracking(color, 0);
     }
 
-    public boolean isFriendly() {
-        return !(getEffect().getCategory().equals(StatusEffectCategory.HARMFUL));
-    }
-    @Override
-    public boolean cannotDespawn() {
-        return super.cannotDespawn() || isFriendly();
-    }
     @Override
     public boolean isAffectedByDaylight() {
-        return super.isAffectedByDaylight() && !isFriendly();
+        return false;
     }
 
     @Override
@@ -103,8 +118,7 @@ public class DimensionalSkeleton extends SkeletonEntity implements TintableEntit
             if (!player.getAbilities().creativeMode) {
                 itemStack.decrement(1);
             }
-            Random r = new Random();
-            if (r.nextFloat() < 0.5) {
+            if (player.getWorld().getRandom().nextFloat() < 0.5) {
                 DimensionalSkeleton newSkeleton;
                 if (!this.getWorld().isClient() && (newSkeleton = ModEntities.DIMENSIONAL_SKELETON.get().create(this.getWorld())) != null) {
                     ((ServerWorld)this.getWorld()).spawnParticles(ParticleTypes.HEART, this.getX(), this.getBodyY(0.5), this.getZ(), 1, 0.0, 0.0, 0.0, 0.0);
@@ -121,7 +135,7 @@ public class DimensionalSkeleton extends SkeletonEntity implements TintableEntit
             }
         }
         if (itemStack.isOf(Items.GLASS_BOTTLE)) {
-            ItemStack itemStack2 = setPotion(Items.LINGERING_POTION.getDefaultStack(), this.getEffectRawId(), this.getDuration() * 60);
+            ItemStack itemStack2 = setPotion(Items.POTION.getDefaultStack(), this.getEffectRawId(), this.getDuration() * 20);
             ItemStack itemStack3 = ItemUsage.exchangeStack(itemStack, player, itemStack2, false);
             player.setStackInHand(hand, itemStack3);
             this.playSound(SoundEvents.ENTITY_COW_MILK, 1.0f, 1.0f);
@@ -184,6 +198,7 @@ public class DimensionalSkeleton extends SkeletonEntity implements TintableEntit
         effects.add(compound);
         PotionUtil.setPotion(stack, Potions.WATER);
         stack.getOrCreateNbt().put("CustomPotionEffects", effects);
+        stack.setCustomName(Text.translatable("potion.infinity.skeleton").setStyle(Style.EMPTY.withItalic(false)));
         return stack;
     }
 
