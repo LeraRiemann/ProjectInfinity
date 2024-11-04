1ba03c7004ac3bd7243b11bdc2bd5124d08924af
Cassian Godsted <cassian@godsted.com>
2024-11-04 13:23:03 -0500

    Initial compatibility with Gravity Changer

---
+++ common/src/main/java/net/lerariemann/infinity/mixin/InfinityMixinPlugin.java
@@ -0,0 +1,48 @@
+package net.lerariemann.infinity.mixin;
+
+import net.lerariemann.infinity.PlatformMethods;
+import org.apache.logging.log4j.LogManager;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class InfinityMixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if(mixinClassName.contains("net.lerariemann.infinity.mixin.mavity") && PlatformMethods.isModLoaded("gravity_changer_q")){
+            LogManager.getLogger().info("The mod \"Gravity Changer\" has been detected. This mod is not completely compatible with Infinite Dimensions. Mavity has been disabled.");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+}
