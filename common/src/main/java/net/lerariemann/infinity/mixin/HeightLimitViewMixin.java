1e63785ee3d6203e0578465338f2db4558acf93d
Cassian Godsted <cassian@godsted.com>
2024-11-30 00:58:52 -0500

    Backport aquifer mixin

--- common/src/main/java/net/lerariemann/infinity/mixin/HeightLimitViewMixin.java
+++ common/src/main/java/net/lerariemann/infinity/mixin/HeightLimitViewMixin.java
@@ -1,16 +1,16 @@
 package net.lerariemann.infinity.mixin;
 
-import net.lerariemann.infinity.InfinityMod;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.HeightLimitView;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(HeightLimitView.class)
 public interface HeightLimitViewMixin {
+
+    /* for some reason minecraft's own code can softlock the game when beating the dragon without this check */
     @Inject(method = "isOutOfHeightLimit(Lnet/minecraft/util/math/BlockPos;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/BlockPos;getY()I"), cancellable = true)
     private void inj(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
         if (pos == null) {
