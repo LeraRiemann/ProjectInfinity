1a62bf56cecaa78c3481eaa11a995a1a1f1f0aea
Lera Riemann <leramipt@gmail.com>
2024-12-01 21:17:55 +0100

    better capitalization

--- common/src/main/java/net/lerariemann/infinity/item/TransfiniteKeyItem.java
+++ common/src/main/java/net/lerariemann/infinity/item/TransfiniteKeyItem.java
@@ -22,24 +22,24 @@ public class TransfiniteKeyItem extends Item {
     public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType type) {
         super.appendTooltip(stack, context, tooltip, type);
         Identifier dimension = stack.getComponents().get(ModItemFunctions.KEY_DESTINATION.get());
+        MutableText mutableText;
         if (dimension != null) {
-            MutableText mutableText;
             String s = dimension.toString();
             if (s.contains("infinity:generated_")) mutableText
                     = Text.translatable("dimension.infinity.generated")
                     .append(s.replace("infinity:generated_", ""));
             else if (s.equals("minecraft:random")) mutableText = Text.translatable("dimension.infinity.randomise");
             else {
-                String fallback = dimension.getPath().replace("_", " ");
-                fallback = Character.toUpperCase(fallback.charAt(0)) + fallback.substring(1);
+                String[] forFallback = dimension.getPath().replace("_", " ").split("\\s");
+                StringBuilder fallback = new StringBuilder();
+                for (String str: forFallback) fallback.append(Character.toUpperCase(str.charAt(0))).append(str.substring(1)).append(" ");
                 mutableText = MutableText.of(new TranslatableTextContent(Util.createTranslationKey("dimension", dimension),
-                        fallback, TranslatableTextContent.EMPTY_ARGUMENTS));
+                        fallback.toString().trim(), TranslatableTextContent.EMPTY_ARGUMENTS));
             }
-            tooltip.add(mutableText.formatted(Formatting.GRAY));
         }
         else {
-            MutableText mutableText = Text.translatable("dimension.infinity.randomise");
-            tooltip.add(mutableText.formatted(Formatting.GRAY));
+            mutableText = Text.translatable("dimension.infinity.randomise");
         }
+        tooltip.add(mutableText.formatted(Formatting.GRAY));
     }
 }
