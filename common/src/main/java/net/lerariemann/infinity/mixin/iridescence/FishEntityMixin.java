c3ee00d02c7a38ba3e4318762a4a0bc51d027d04
Lera <lerariemann@gmail.com>
2024-12-16 21:04:53 +0100

    Revert "Disable Fish Move Control mixin without Mixin Booster"
    This reverts commit c7905d6975e5ac71787ae4a2d4f4d5a13de179dd.

--- common/src/main/java/net/lerariemann/infinity/mixin/iridescence/FishEntityMixin.java
+++ common/src/main/java/net/lerariemann/infinity/mixin/iridescence/FishEntityMixin.java
@@ -31,4 +31,26 @@ public class FishEntityMixin extends WaterCreatureEntity {
         }
         return par2;
     }
+
+    @Mixin(FishEntity.FishMoveControl.class)
+    public static class FishMoveControlMixin extends MoveControl {
+        @Final
+        @Shadow
+        private FishEntity fish;
+
+        public FishMoveControlMixin(MobEntity entity) {
+            super(entity);
+        }
+
+        @ModifyArgs(method = "tick", at= @At(value = "INVOKE",
+                target = "Lnet/minecraft/util/math/MathHelper;atan2(DD)D"))
+        void inj(Args args) {
+            if (Iridescence.isUnderEffect(fish)) {
+                double g = args.get(0);
+                double d = args.get(1);
+                args.set(0, -1*g);
+                args.set(1, -1*d);
+            }
+        }
+    }
 }
