bbce0cdd928935cd013531ca63705c891d1ed05d
Lera <lerariemann@gmail.com>
2025-01-01 18:12:02 +0100

    got the duration :3

--- common/src/main/java/net/lerariemann/infinity/util/config/SoundScanner.java
+++ common/src/main/java/net/lerariemann/infinity/util/config/SoundScanner.java
@@ -9,6 +9,9 @@ import net.minecraft.resource.Resource;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Stream;
@@ -53,7 +56,13 @@ public record SoundScanner(Map<Identifier, Resource> soundIds) {
                     subtitlesForRP.putString(subtitleID, subtitleData);
                     soundsForRP.put(songID, soundForRP); //resourcepack side
 
-                    CommonIO.write(getJukeboxDef(songID, subtitleID),
+                    double length;
+                    try {
+                        length = calculateDuration(soundIds.get(id).getInputStream().readAllBytes());
+                    } catch (IOException e) {
+                        length = 600;
+                    }
+                    CommonIO.write(getJukeboxDef(songID, subtitleID, length), //datapack side
                             savingPath.resolve("datapacks/infinity/data/infinity/jukebox_song"),
                             arr.getLast() + ".json");
                 });
@@ -61,7 +70,48 @@ public record SoundScanner(Map<Identifier, Resource> soundIds) {
         CommonIO.write(subtitlesForRP, savingPath.resolve("resourcepacks/infinity/assets/infinity/lang"), "en_us.json");
     }
 
-    private static @NotNull NbtCompound getJukeboxDef(String songID, String subtitleID) {
+    /**
+     Getting a duration of an OGG file from its byte data; <a href="https://stackoverflow.com/a/44407355">implementation from StackOverflow</a>.
+     */
+    public static double calculateDuration(byte[] track) throws IOException {
+        int rate = -1;
+        int length = -1;
+        int size = track.length;
+
+        for (int i = size-1-8-2-4; i>=0 && length<0; i--) { //4 bytes for "OggS", 2 unused bytes, 8 bytes for length
+            // Looking for length (value after last "OggS")
+            if (
+                    track[i]==(byte)'O'
+                            && track[i+1]==(byte)'g'
+                            && track[i+2]==(byte)'g'
+                            && track[i+3]==(byte)'S'
+            ) {
+                byte[] byteArray = new byte[]{track[i+6],track[i+7],track[i+8],track[i+9],track[i+10],track[i+11],track[i+12],track[i+13]};
+                ByteBuffer bb = ByteBuffer.wrap(byteArray);
+                bb.order(ByteOrder.LITTLE_ENDIAN);
+                length = bb.getInt(0);
+            }
+        }
+        for (int i = 0; i<size-8-2-4 && rate<0; i++) {
+            // Looking for rate (first value after "vorbis")
+            if (
+                    track[i]==(byte)'v'
+                            && track[i+1]==(byte)'o'
+                            && track[i+2]==(byte)'r'
+                            && track[i+3]==(byte)'b'
+                            && track[i+4]==(byte)'i'
+                            && track[i+5]==(byte)'s'
+            ) {
+                byte[] byteArray = new byte[]{track[i+11],track[i+12],track[i+13],track[i+14]};
+                ByteBuffer bb = ByteBuffer.wrap(byteArray);
+                bb.order(ByteOrder.LITTLE_ENDIAN);
+                rate = bb.getInt(0);
+            }
+        }
+        return length / (double) rate;
+    }
+
+    private static @NotNull NbtCompound getJukeboxDef(String songID, String subtitleID, double length) {
         NbtCompound jukebox_def = new NbtCompound();
         NbtCompound sound_event = new NbtCompound();
         sound_event.putString("sound_id", "infinity:" + songID);
@@ -69,7 +119,7 @@ public record SoundScanner(Map<Identifier, Resource> soundIds) {
         NbtCompound description = new NbtCompound();
         description.putString("translate", subtitleID);
         jukebox_def.put("description", description);
-        jukebox_def.putFloat("length_in_seconds", 600);
+        jukebox_def.putFloat("length_in_seconds", (float)length);
         jukebox_def.putInt("comparator_output", 15);
         return jukebox_def;
     }
