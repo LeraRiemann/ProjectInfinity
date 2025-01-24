18bff27332214683e4c974a7601cbfb98658dc24
Cassian Godsted <cassian@godsted.com>
2025-01-24 11:43:27 -0500

    Update ServerPlayerEntityMixin.java

--- forge/src/main/java/net/lerariemann/infinity/mixin/forge/ServerPlayerEntityMixin.java
+++ forge/src/main/java/net/lerariemann/infinity/mixin/forge/ServerPlayerEntityMixin.java
@@ -5,9 +5,8 @@ import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.lerariemann.infinity.access.ServerPlayerEntityAccess;
 import net.lerariemann.infinity.registry.var.ModPayloads;
-import net.lerariemann.infinity.util.PortalCreationLogic;
-import net.lerariemann.infinity.var.ModPayloads;
-import net.lerariemann.infinity.var.ModStats;
+import net.lerariemann.infinity.registry.var.ModStats;
+import net.lerariemann.infinity.util.teleport.PortalCreator;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
@@ -41,7 +40,7 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity implements Se
 
     @Inject(method = "lambda$changeDimension$8", at= @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;setServerWorld(Lnet/minecraft/server/world/ServerWorld;)V"))
     private void convertReturnPortal(ServerWorld world, RegistryKey<World> registryKey, ServerWorld destination, TeleportTarget teleportTarget, Boolean spawnPortal, CallbackInfoReturnable<Entity> cir) {
-        boolean bl = PortalCreationLogic.convertReturnPortal(destination, server, registryKey, teleportTarget);
+        boolean bl = PortalCreator.convertReturnPortal(destination, server, registryKey, teleportTarget);
         if (bl) {
             this.increaseStat(ModStats.PORTALS_OPENED_STAT, 1);
         }
