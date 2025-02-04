4d863c1d5a260962bbdec76e58315f2460c2621c
Lera <lerariemann@gmail.com>
2025-02-04 06:42:55 +0100

    a bit of documentation

--- common/src/main/java/net/lerariemann/infinity/util/ClientMethods.java
+++ common/src/main/java/net/lerariemann/infinity/util/ClientMethods.java
@@ -5,6 +5,8 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 
+/** Mod methods that should only be loaded on the client (trying to load these on the server crashes it, don't do that).
+ * @see InfinityMethods */
 public class ClientMethods {
     public static void setF4Screen(PlayerEntity player) {
         if (player instanceof ClientPlayerEntity clientPlayer) {
