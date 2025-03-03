a440402b52ae00ee7a47a45b3b4c73ddad5057e7
Lera <lerariemann@gmail.com>
2025-03-03 04:29:33 +0100

    let's move that to the main mod class, still doesn't fix anything tho

--- neoforge/src/main/java/net/lerariemann/infinity/neoforge/InfinityModNeoForge.java
+++ neoforge/src/main/java/net/lerariemann/infinity/neoforge/InfinityModNeoForge.java
@@ -8,17 +8,25 @@ import net.lerariemann.infinity.fluids.neoforge.ModEffectsNeoforge;
 import net.lerariemann.infinity.fluids.neoforge.ModFluidsNeoforge;
 import net.lerariemann.infinity.registry.core.ModItemFunctions;
 import net.lerariemann.infinity.neoforge.client.InfinityModNeoForgeClient;
+import net.lerariemann.infinity.registry.var.ModPayloads;
 import net.lerariemann.infinity.registry.var.ModStats;
+import net.lerariemann.infinity.util.config.SoundScanner;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.EventBus;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
+import net.neoforged.neoforge.network.handling.IPayloadContext;
+import net.neoforged.neoforge.network.registration.PayloadRegistrar;
+
+import static net.lerariemann.infinity.registry.var.ModPayloads.*;
+import static net.lerariemann.infinity.registry.var.ModPayloads.receiveF4DeployPayload;
 
 @Mod(InfinityMod.MOD_ID)
 public final class InfinityModNeoForge {
@@ -48,4 +56,55 @@ public final class InfinityModNeoForge {
         ModItemFunctions.registerDispenserBehaviour();
     }
 
+    public static MinecraftClient client() {
+        return MinecraftClient.getInstance();
+    }
+    public static ServerPlayerEntity getPlayer(IPayloadContext context) {
+        return ((ServerPlayerEntity)context.player());
+    }
+
+    @SubscribeEvent
+    public static void registerPayloads(final RegisterPayloadHandlersEvent event) {
+        final PayloadRegistrar registrar = event.registrar("1");
+        registrar.playToClient(
+                ModPayloads.WorldAddS2CPayload.ID,
+                ModPayloads.WorldAddS2CPayload.CODEC,
+                (payload, context) -> receiveWorldAddPayload(client(), payload.world_id(), payload.world_data())
+        );
+        registrar.playToClient(
+                ModPayloads.BiomeAddS2CPayload.ID,
+                ModPayloads.BiomeAddS2CPayload.CODEC,
+                (payload, context) -> receiveBiomeAddPayload(client(), payload.biome_id(), payload.biome_data())
+        );
+        registrar.playToClient(
+                ModPayloads.ShaderS2CPayload.ID,
+                ModPayloads.ShaderS2CPayload.CODEC,
+                (payload, context) -> receiveShaderPayload(client(), payload.shader_data(), payload.iridescence())
+        );
+        registrar.playToClient(
+                ModPayloads.StarsS2CPayload.ID,
+                ModPayloads.StarsS2CPayload.CODEC,
+                (payload, context) -> receiveStarsPayload(client())
+        );
+        registrar.playToClient(
+                ModPayloads.SoundPackS2CPayload.ID,
+                ModPayloads.SoundPackS2CPayload.CODEC,
+                (payload, context) -> SoundScanner.unpackDownloadedPack(payload.songIds(), client())
+        );
+        registrar.playToServer(
+                ModPayloads.F4UpdateC2SPayload.ID,
+                ModPayloads.F4UpdateC2SPayload.CODEC,
+                (payload, context) -> receiveF4UpdatePayload(getPlayer(context), payload.slot(), payload.width(), payload.height())
+        );
+        registrar.playToServer(
+                ModPayloads.F4DeployC2SPayload.ID,
+                ModPayloads.F4DeployC2SPayload.CODEC,
+                (payload, context) -> receiveF4DeployPayload(getPlayer(context))
+        );
+        registrar.playToServer(
+                ModPayloads.JukeboxesC2SPayload.ID,
+                ModPayloads.JukeboxesC2SPayload.CODEC,
+                (payload, context) -> SoundScanner.unpackUploadedJukeboxes(getPlayer(context).server, payload.data())
+        );
+    }
 }
