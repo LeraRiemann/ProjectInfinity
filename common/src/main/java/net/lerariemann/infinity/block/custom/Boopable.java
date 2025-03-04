49f5e59b464e53fe07c409601301f69c5a54fffc
Lera <lerariemann@gmail.com>
2025-03-04 03:32:21 +0100

    boopable wool

--- common/src/main/java/net/lerariemann/infinity/block/custom/Boopable.java
+++ common/src/main/java/net/lerariemann/infinity/block/custom/Boopable.java
@@ -4,6 +4,8 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public interface Boopable {
     BooleanProperty BOOP = BooleanProperty.of("boop");
@@ -12,5 +14,9 @@ public interface Boopable {
     }
     default void appendBoop(StateManager.Builder<Block, BlockState> builder) {
         builder.add(BOOP);
+    }
+    static void boop(World w, BlockPos pos) {
+        BlockState bs = w.getBlockState(pos);
+        if (bs.contains(BOOP)) w.setBlockState(pos, bs.with(BOOP, !bs.get(BOOP)));
     }
 }
