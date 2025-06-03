2d69dfa2a595a9be22a093863d22876d0bd7e410
Cassian Godsted <cassian@godsted.com>
2025-06-03 13:22:41 -0400

    null safety!

--- common/src/main/java/net/lerariemann/infinity/compat/cloth/AmendmentConfigFactory.java
+++ common/src/main/java/net/lerariemann/infinity/compat/cloth/AmendmentConfigFactory.java
@@ -21,13 +21,6 @@ import java.util.*;
 import static net.lerariemann.infinity.compat.cloth.ClothConfigFactory.*;
 
 public class AmendmentConfigFactory {
-    enum AreasEnum {
-        BLOCKS,
-        FLUIDS,
-        ITEMS,
-        STRUCTURES,
-        MOBS;
-    }
 
     public static void build(ConfigBuilder builder) {
 
@@ -37,32 +30,51 @@ public class AmendmentConfigFactory {
         var elements = amendmentList.getAsJsonArray("elements");
         int i = 0;
         for (JsonElement amendmentElement : elements) {
-            SubCategoryBuilder subCategory = builder.entryBuilder().startSubCategory(Text.translatable("config.infinity.amendment", String.valueOf(i)));
-            JsonObject amendment = amendmentElement.getAsJsonObject();
-            addStringDropdownOption("area", builder, subCategory, i, amendment, Lists.newArrayList("blocks", "fluids", "items", "structures", "trees", "mobs"));
-            addStringOption("mod", builder, subCategory, i, amendment);
-            addStringDropdownOption("selector", builder, subCategory, i, amendment, Lists.newArrayList("all", "matching", "matching_any", "matching_block_tag", "containing"));
-            if (amendment.get("selector").getAsString().equals("matching_any"))
-                addListOption("matching", builder, subCategory, i, amendment);
-            else if (amendment.get("selector").getAsString().equals("matching")) {
-                addStringOption("matching", builder, subCategory, i, amendment);
-            }
-            addStringDropdownOption("results", builder, subCategory, i, amendment, Lists.newArrayList("set_value", "set_field", "erase"));
-            if (amendment.get("results").getAsString().equals("set_value"))
-                addDoubleOption("value", builder, subCategory, i, amendment);
-            else if (amendment.get("results").getAsString().equals("set_field"))
-                addStringDropdownOption("field_name", builder, subCategory, i, amendment, Lists.newArrayList("full", "float", "top", "laggy"));
-
-            amendmentCategory.addEntry(subCategory.build());
-            i++;
+            i = addAmendment(builder, amendmentElement, i, amendmentCategory);
         }
+        // todo new amendments
+//        addAmendment(builder, null, i, amendmentCategory);
         amendmentCategory.addEntry(builder.entryBuilder().startTextDescription(Text.literal("To add new entries, edit amendments.json.").setStyle(Style.EMPTY)).build());
     }
 
+    private static int addAmendment(ConfigBuilder builder, JsonElement amendmentElement, int i, ConfigCategory amendmentCategory) {
+        JsonObject amendment;
+        SubCategoryBuilder subCategory;
+        if (amendmentElement != null) {
+            subCategory = builder.entryBuilder().startSubCategory(Text.translatable("config.infinity.amendment", String.valueOf(i)));
+            amendment = amendmentElement.getAsJsonObject();
+        } else {
+            subCategory = builder.entryBuilder().startSubCategory(Text.translatable("config.infinity.amendment.new"));
+            amendment = new JsonObject();
+        }
+        addStringDropdownOption("area", builder, subCategory, i, amendment, Lists.newArrayList("blocks", "fluids", "items", "structures", "trees", "mobs"));
+        addStringOption("mod", builder, subCategory, i, amendment);
+        addStringDropdownOption("selector", builder, subCategory, i, amendment, Lists.newArrayList("all", "matching", "matching_any", "matching_block_tag", "containing"));
+        if (getSafeString(amendment, "selector").equals("matching_any"))
+            addListOption("matching", builder, subCategory, i, amendment);
+        else if (getSafeString(amendment, "selector").equals("matching")) {
+            addStringOption("matching", builder, subCategory, i, amendment);
+        }
+        addStringDropdownOption("results", builder, subCategory, i, amendment, Lists.newArrayList("set_value", "set_field", "erase"));
+        if (getSafeString(amendment, "results").equals("set_value"))
+            addDoubleOption("value", builder, subCategory, i, amendment);
+        else if (getSafeString(amendment, "results").equals("set_field"))
+            addStringDropdownOption("field_name", builder, subCategory, i, amendment, Lists.newArrayList("full", "float", "top", "laggy"));
+
+        amendmentCategory.addEntry(subCategory.build());
+        i++;
+        return i;
+    }
+
+    private static String getSafeString(JsonObject amendment, String key) {
+        var amend = amendment.get(key);
+        if (amend != null)
+            return amend.getAsString();
+        else return "";
+    }
+
     private static void addStringOption(String name, ConfigBuilder builder, SubCategoryBuilder subCategory, int i, JsonObject amendment) {
-        String current;
-        if (amendment.get(name) == null) current = "";
-        else current =  amendment.get(name).getAsString();
+        String current = getSafeString(amendment, name);
         subCategory.add(builder.entryBuilder().startStrField(
                         Text.translatable("config.infinity.amendments."+name),
                         current)
@@ -72,9 +84,7 @@ public class AmendmentConfigFactory {
     }
 
     private static void addStringDropdownOption(String name, ConfigBuilder builder, SubCategoryBuilder subCategory, int i, JsonObject amendment, List<String> options) {
-        String current;
-        if (amendment.get(name) == null) current = "";
-        else current =  amendment.get(name).getAsString();
+        String current = getSafeString(amendment, name);
         subCategory.add(builder.entryBuilder().startDropdownMenu(Text.translatable("config.infinity.amendments."+name), DropdownMenuBuilder.TopCellElementBuilder.of(current, (s) -> s))
                 .setTooltip(amendmentTooltip(name))
                 .setSuggestionMode(false)
