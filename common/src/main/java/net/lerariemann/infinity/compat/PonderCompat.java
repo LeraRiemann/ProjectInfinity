63553b7714bd6633b11d555b58906c441c7e055f
Cassian Godsted <cassian@godsted.com>
2025-06-18 11:10:23 -0400

    merge upstream

--- common/src/main/java/net/lerariemann/infinity/compat/PonderCompat.java
+++ common/src/main/java/net/lerariemann/infinity/compat/PonderCompat.java
@@ -1,10 +1,9 @@
 package net.lerariemann.infinity.compat;
 
-import net.createmod.ponder.api.level.PonderLevel;
 import net.minecraft.world.World;
 
 public class PonderCompat {
     public static boolean isPonderLevel(World world) {
-        return world instanceof PonderLevel;
+        return false;
     }
 }
